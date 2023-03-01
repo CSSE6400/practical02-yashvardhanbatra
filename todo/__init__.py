d3924a6412ec00d54c6d7090599aa10604781b2b
Yashvardhan Batra <yashvardhanbatra410@gmail.com>
2023-03-01 12:33:33 +1000

    commit

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
 
-    from .views.routes import api
-    app.register_blueprint(api)
+# Load the models
+from todo.models import db
+from todo.models.todo import Todo
+db.init_app(app)
+    
+    # Create the database tables
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
-    return app
+# Register the blueprints
+from todo.views.routes import api
+app.register_blueprint(api)
+
+return app
